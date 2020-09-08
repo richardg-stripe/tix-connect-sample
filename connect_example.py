9d3769d3bb349b3b6913ca365e1fb7f4a50d1f6c
Richard Gill <richardg@stripe.com>
2020-09-08 17:50:46 +0100

    tabs -> spaces

--- connect_example.py
+++ connect_example.py
@@ -19,34 +19,34 @@ payment_intent = stripe.PaymentIntent.create(
   payment_method_types = ["card"],
   payment_method = "pm_card_gb", # Test card!
   confirm = True,
-	# on_behalf_of=supplier_1.id     # This can be used to change the Merchant of Record to be Supplier 1.
+  # on_behalf_of=supplier_1.id     # This can be used to change the Merchant of Record to be Supplier 1.
 )
 
 def calculatePlatformMargin():
-	return 300 # Your logic goes here!!
+  return 300 # Your logic goes here!!
 
 print(payment_intent)
 charge_id = payment_intent['charges']['data'][0]['id']
 supplier_1_transfer = stripe.Transfer.create(
-	amount = 5000 - calculatePlatformMargin(),
-	currency = "gbp",
-	destination = supplier_1.id,
-	source_transaction = charge_id # This allows you to do the transfer immediately, before the funds become 'available' in your Stripe balance
+  amount = 5000 - calculatePlatformMargin(),
+  currency = "gbp",
+  destination = supplier_1.id,
+  source_transaction = charge_id # This allows you to do the transfer immediately, before the funds become 'available' in your Stripe balance
 )
 
 # Seperate charges and transfers Docs: https://stripe.com/docs/connect/charges-transfers
 
 supplier_2_transfer = stripe.Transfer.create(
-	amount = 5000 - calculatePlatformMargin(),
-	currency = "gbp",
-	destination = supplier_2.id,
-	source_transaction = charge_id # This allows you to do the transfer immediately, before the funds become 'available' in your Stripe balance
+  amount = 5000 - calculatePlatformMargin(),
+  currency = "gbp",
+  destination = supplier_2.id,
+  source_transaction = charge_id # This allows you to do the transfer immediately, before the funds become 'available' in your Stripe balance
 )
 
 # Refund supplier 2's part of the transaction
 stripe.Refund.create(
   charge = charge_id,
-	amount = 5000
+  amount = 5000
 )
 
 # Claw back funds from supplier 2's Stripe account, since they are still there, this succeeds.
